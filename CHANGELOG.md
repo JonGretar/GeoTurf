59f8de2206177f1648c115e754700874660c5720
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-03-05 14:40:05 +0200

    version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,12 @@
 # Geo Turf Changelog
 
+## 0.2.0
+
+ * Added Measure.area/1
+ * Added Measure.bearing/2
+ * Fixed a bad mod/2 error.
+ * Library cleanup
+
 ## 0.1.0
+
 First Release...
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Geo.Turf.MixProject do
   def project do
     [
       app: :geo_turf,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       # start_permanent: Mix.env() == :prod,
       description: description(),
