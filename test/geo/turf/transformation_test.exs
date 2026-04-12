cfe6269dd11b76f3cfe0fa19005b885721372a03
Jón Grétar Borgþórsson <jon@montel.fi>
2026-04-12 15:45:02 +0300

    Make the tests round polygons because of mismatching floating point handling between OTP versions

--- test/geo/turf/transformation_test.exs
+++ test/geo/turf/transformation_test.exs
@@ -8,6 +8,16 @@ defmodule Geo.Test.TransformationTest do
   test "circle", ctx do
     [center, result_circle] = ctx.circle
 
-    assert T.circle(center, 1, steps: 10, unit: :kilometers) == result_circle
+    assert round_polygon(T.circle(center, 1, steps: 10, unit: :kilometers)) ==
+             round_polygon(result_circle)
+  end
+
+  defp round_polygon(%Geo.Polygon{coordinates: coords} = polygon) do
+    rounded =
+      Enum.map(coords, fn ring ->
+        Enum.map(ring, fn {x, y} -> {Float.round(x, 10), Float.round(y, 10)} end)
+      end)
+
+    %{polygon | coordinates: rounded}
   end
 end
