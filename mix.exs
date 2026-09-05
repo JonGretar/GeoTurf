9480e00101d842385be62c2ac59df156987b9ce2
Jón Grétar Borgþórsson <jon@montel.fi>
2026-09-05 03:24:43 +0300

    release: prepare 0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Geo.Turf.MixProject do
   def project do
     [
       app: :geo_turf,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.15",
       # start_permanent: Mix.env() == :prod,
       description: description(),
